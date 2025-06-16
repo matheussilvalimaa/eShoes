97e8a9413306256a07a72c7f3eaadf5d505a209a
Matheus Lima <silvalimamatheus@outlook.com>
2025-06-16 18:06:42 -0300

    Update CheckoutResponse.cs

--- eShoesBackend/src/Models/DTO/CheckoutResponse.cs
+++ eShoesBackend/src/Models/DTO/CheckoutResponse.cs
@@ -5,7 +5,6 @@ namespace eShoes.DTO
         public string Last4        { get; set; }
         public string PixQrCode    { get; set; }
         public string PixCode      { get; set; }
-        public string BoletoUrl    { get; set; }
         public decimal ShippingCost { get; set; }
         public decimal TotalAmount  { get; set; }
     }
